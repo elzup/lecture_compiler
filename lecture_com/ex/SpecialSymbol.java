e753abc904e320dd91afaf5621a46df1d3822eb6
Hiroto Takahashi <guild0105@gmail.com>
2013-12-04 11:53:45 +0900

    indent整形

--- lecture_com/ex/SpecialSymbol.java
+++ lecture_com/ex/SpecialSymbol.java
@@ -2,105 +2,112 @@ package ex;
 
 // 特殊記号 ^ ; =
 public class SpecialSymbol extends Token {
-    public SpecialSymbol(String string) {
-	super(string);
-	if     (string.equals("^")) kind = START;
-	else if(string.equals(";")) kind = END;
-	// "=" は扱わないので何もしない
-    }
+	public SpecialSymbol(String string) {
+		super(string);
+		if (string.equals("^"))
+			kind = START;
+		else if (string.equals(";"))
+			kind = END;
+		// "=" は扱わないので何もしない
+	}
 }
 
 // 2項演算子の抽象クラス。2引数の calc メソッドを持つ
 abstract class BinaryOperator extends Token {
-    public BinaryOperator(String string) {
-	super(string);
-    }
-    public abstract int calc(int operand1, int operand2);
+	public BinaryOperator(String string) {
+		super(string);
+	}
+
+	public abstract int calc(int operand1, int operand2);
 }
 
 // 2項演算子 +
 class Sum extends BinaryOperator {
-    public Sum(String string) {
-	super(string);
-	kind = SUM;
-    }
-    public int calc(int operand1, int operand2) {
-	return operand1 + operand2;
-    }
+	public Sum(String string) {
+		super(string);
+		kind = SUM;
+	}
+
+	public int calc(int operand1, int operand2) {
+		return operand1 + operand2;
+	}
 }
 
 // 2項演算子 -
 class Sub extends BinaryOperator {
-    public Sub(String string) {
-	super(string);
-	kind = SUB;
-    }
-    public int calc(int operand1, int operand2) {
-	return operand1 - operand2;
-    }
+	public Sub(String string) {
+		super(string);
+		kind = SUB;
+	}
+
+	public int calc(int operand1, int operand2) {
+		return operand1 - operand2;
+	}
 }
 
 // 2項演算子 *
 class Mul extends BinaryOperator {
-    public Mul(String string) {
-	super(string);
-	kind = MUL;
-    }
-    public int calc(int operand1, int operand2) {
-	return operand1 * operand2;
-    }
+	public Mul(String string) {
+		super(string);
+		kind = MUL;
+	}
+
+	public int calc(int operand1, int operand2) {
+		return operand1 * operand2;
+	}
 }
 
 // 2項演算子 /
 class Div extends BinaryOperator {
-    public Div(String string) {
-	super(string);
-	kind = DIV;
-    }
-    public int calc(int operand1, int operand2) {
-	return operand1 / operand2;
-    }
+	public Div(String string) {
+		super(string);
+		kind = DIV;
+	}
+
+	public int calc(int operand1, int operand2) {
+		return operand1 / operand2;
+	}
 }
 
 // 左括弧
 class LParen extends Token {
-    public LParen(String string) {
-	super(string);
-	kind = LPAREN;
-    }
+	public LParen(String string) {
+		super(string);
+		kind = LPAREN;
+	}
 }
 
 // 右括弧
 class RParen extends Token {
-    public RParen(String string) {
-	super(string);
-	kind = RPAREN;
-    }
+	public RParen(String string) {
+		super(string);
+		kind = RPAREN;
+	}
 }
 
 // 整数定数
 class IntConstant extends Token {
-    private int value;
-    public IntConstant(String string) {
-	super(string);
-	try {
-	    value = Integer.parseInt(string);
+	private int value;
+
+	public IntConstant(String string) {
+		super(string);
+		try {
+			value = Integer.parseInt(string);
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+		kind = OPERAND;
 	}
-	catch(Exception e) {
-	    System.err.println(e);
+
+	public int intValue() {
+		return value;
 	}
-	kind = OPERAND;
-    }
-    public int intValue() {
-	return value;
-    }
 }
 
 // 識別子(変数名)
 class Identifier extends Token {
-    public Identifier(String string) {
-	super(string);
-	kind = OPERAND;
-    }
+	public Identifier(String string) {
+		super(string);
+		kind = OPERAND;
+	}
 }
-
